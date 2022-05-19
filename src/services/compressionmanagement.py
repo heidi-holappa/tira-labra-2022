82ac35f12b301c866f7e4373bbd7e5b70b2ec91e
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-05-19 22:24:45 +0300

    initial commit

---
+++ src/services/compressionmanagement.py
@@ -0,0 +1,25 @@
+class CompressionManagement:
+
+    def __init__(self):
+        pass
+
+    def initial_huffman_test(self, content: str):
+        """A method for testing Huffman coding. 
+        
+        Gets a string of content and compresses it. 
+
+        Args:
+            content (str): content to be compressed
+        """
+        pass
+
+    def initial_lempel_ziv_test(self, content: str):
+        """A method for testing Lempel-Ziv 77 compression. 
+
+        Gets a string of content and compresses it. 
+
+        Args:
+            content (str): content to be compressed
+        """
+
+        pass
