a2d831db43c0040cec052f0218344ad9f39e9e7b
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-05-22 23:19:20 +0300

    Refactor code

--- src/tests/huffman_compression_test.py
+++ src/tests/huffman_compression_test.py
@@ -76,8 +76,6 @@ class TestHuffmanCompression(unittest.TestCase):
         self.huffman_coder.uncompressed_filename = uncompressed_filename
         self.huffman_coder.execute_uncompression()
         content_matches = True
-        print("original content: ", content, ", uncompressed content: ",
-              self.huffman_coder.uncompressed)
         os.remove(uncompressed_filename)
         for i in range(len(content)):
             if content[i] != self.huffman_coder.uncompressed[i]:
