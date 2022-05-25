0acc4c3fd97e9f07ff06a7355ba0b521b690335f
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-05-25 15:51:39 +0300

    Add first working version of compression

--- src/entities/lempelziv77.py
+++ src/entities/lempelziv77.py
@@ -1,7 +1,5 @@
 # STILL A DRAFT. BUILDING THIS ON WEEK 3
-
 import string
-
 
 class LempelZiv77:
 
@@ -33,17 +31,16 @@ class LempelZiv77:
         while i < len(self.content):
             result = self.init_window_search(i)
             compressed_content_as_list.append(result)
-            i += 1
-        print(compressed_content_as_list)
+            i += result[1]
+        for i in compressed_content_as_list:
+            print(i)
 
     def init_window_search(self, current_index: int) -> tuple:
         window_start_index = max(0, current_index - self.window_size)
         window_end_index = current_index
         buffer_end_index = min(current_index + self.buffer_size, len(self.content))
-        result = self.find_longest_match(current_index, self.content[window_start_index:window_end_index], self.content[window_end_index:buffer_end_index])
+        result = self.find_longest_match(current_index, self.content[window_start_index:window_end_index], self.content[current_index:buffer_end_index])
         return result
-        
-
 
     def find_longest_match(self, current_index: int, window: str, buffer: str) -> tuple:
         longest = (0, 0, 0)
@@ -51,18 +48,15 @@ class LempelZiv77:
         i = 0
         while i < len(window):
             # print("Window pointer: ", max(0, current_index - self.window_size) + i, "character: ", self.content[current_index + i], "Buffer: ", current_index + len(window), "buffer_character: ", self.content[current_index + len(window)] )
-            string_length = self.repeating_length_recursion(window[i:], buffer)
-            if string_length > result:
-                result = string_length
+            match_length = self.repeating_length_recursion(window[i:], buffer)
+            if match_length > result:
+                result = match_length
                 offset = len(window) - i
-                length = result
-                next_character = current_index + i + result
+                next_character = current_index + match_length
                 if next_character >= len(self.content):
                     next_character = 0
-                longest = (offset, length, self.content[next_character])
-                i += length
-            else:
-                i += 1
+                longest = (offset, match_length, self.content[next_character])
+            i += 1
         if result == 0:
             longest = (0, 1, self.content[current_index])
         return longest
@@ -71,8 +65,11 @@ class LempelZiv77:
     def repeating_length_recursion(self, window: str, string_buffer: str):
         """A recursion that finds the total length of the string match. 
 
-        This code was taken from Tim Guite's excellent tutorial:
+        This method was referenced from Tim Guite's tutorial:
         https://github.com/TimGuite/lz77/blob/master/python/compress.py
+
+        Maximum recursion depth is the size of the lookahead buffer. Please
+        be aware of this when configuring the buffer value. 
 
         Args:
             window (str): the window from which matches are looked for
