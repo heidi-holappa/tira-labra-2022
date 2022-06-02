3a464b75a8158475eb71171c662cb6768b5ad128
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-06-02 23:04:38 +0300

    Add default directory for extensive test data

--- src/config.py
+++ src/config.py
@@ -9,5 +9,7 @@ except FileNotFoundError:
     print("Warning! .env file not found.")
 
 path = os.getenv('DEFAULT_DATA_DIRECTORY') or 'data'
+testing_path = os.getenv('DEFAULT_EXTENSIVE_TESTING_DIRECTORY') or 'test-data'
 
 DEFAULT_DATA_PATH = os.path.join(dirname, '..', path)
+DEFAULT_TEST_DATA_PATH = os.path.join(dirname, '..', testing_path)
