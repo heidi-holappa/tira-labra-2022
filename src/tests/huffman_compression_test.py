ae4c29217a84686debd043928d54983f7287f1f0
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-06-04 20:54:07 +0300

    Fix broken link

--- src/tests/huffman_compression_test.py
+++ src/tests/huffman_compression_test.py
@@ -122,7 +122,7 @@ class TestHuffmanCompression(unittest.TestCase):
 
     def test_compressed_data_matches_with_a_longer_existing_file(self):
         filename = os.path.join(DEFAULT_TEST_DATA_PATH,
-                                "randon-printable-ascii-data.txt")
+                                "natural-language-document-50-paragraphs.txt")
         with open(filename, "r", encoding="utf-8") as file:
             content = file.read()
         self.create_test_file(content)
