b603d62b6821b46e72ba47f5ac9e44a6d99cf20c
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-06-12 13:29:47 +0300

    Change layout, simplify functionalities.

--- src/gui/gui_compression_view.py
+++ src/gui/gui_compression_view.py
@@ -11,7 +11,8 @@ class CompressionView:
         self._root = root
         self._frame = None
         self._state = "enabled"
-        self.filename = None
+        self.file_to_compress = None
+        self.file_to_uncompress = None
         self.compression_management = default_compression_management
         self.filemanager = default_file_manager
 
@@ -49,15 +50,34 @@ class CompressionView:
             style="Custom.TLabelframe"
         )
 
-        self._buttons_frame = ttk.LabelFrame(
+        self._compression_frame = ttk.LabelFrame(
             master=self._frame,
-            text="Select action",
+            text="Compress",
             style="Custom.TLabelframe"
         )
 
-        self._label_and_instruction_frame.grid(sticky=constants.EW)
-        self._analysis_frame.grid(sticky=constants.EW)
-        self._buttons_frame.grid(sticky=constants.EW)
+        self._uncompression_frame = ttk.LabelFrame(
+            master=self._frame,
+            text="Uncompress",
+            style="Custom.TLabelframe"
+        )
+
+        self._label_and_instruction_frame.grid(
+            row=0,
+            columnspan=2,
+            sticky=constants.EW)
+        self._analysis_frame.grid(
+            row=1,
+            columnspan=2,
+            sticky=constants.EW)
+        self._compression_frame.grid(
+            row=2,
+            column=0,
+            sticky=constants.EW)
+        self._uncompression_frame.grid(
+            row=2,
+            column=1,
+            sticky=constants.EW)
 
         label = ttk.Label(
             master=self._label_and_instruction_frame,
@@ -71,9 +91,15 @@ class CompressionView:
             style="Centered.TLabel"
         )
 
-        self.label_file_explorer = ttk.Label(
-            self._buttons_frame,
-            text="Start by selecting a file to compress / uncompress",
+        self.label_file_explorer_compress = ttk.Label(
+            self._compression_frame,
+            text="Select a file to compress",
+            style="Custom.TLabel"
+        )
+
+        self.label_file_explorer_uncompress = ttk.Label(
+            self._uncompression_frame,
+            text="Select a file to uncompress",
             style="Custom.TLabel"
         )
 
@@ -90,27 +116,66 @@ class CompressionView:
             pady=10
         )
 
-        self.label_file_explorer.grid(
+        self.label_file_explorer_compress.grid(
             row=0,
             column=0,
             pady=10
         )
 
-        self._construct_radio_buttons()
+        self.label_file_explorer_uncompress.grid(
+            row=0,
+            column=0,
+            pady=10
+        )
+
         self._construct_log_frame()
         self._construct_buttons()
 
-    def _construct_radio_buttons(self):
+
+
+
+
+    def _construct_buttons(self):
+
+        button_select_file_to_compress = ttk.Button(
+            master=self._compression_frame,
+            text="select file",
+            command=self._handle_get_file_to_compress,
+            style="Custom.TButton"
+
+        )
+
+        button_select_file_to_uncompress = ttk.Button(
+            master=self._uncompression_frame,
+            text="select file",
+            command=self._handle_get_file_to_uncompress,
+            style="Custom.TButton"
+
+        )
+
+        button_compress = ttk.Button(
+            master=self._compression_frame,
+            text="compress",
+            command=self._handle_compression,
+            style="Custom.TButton"
+        )
+
+        button_uncompress = ttk.Button(
+            master=self._uncompression_frame,
+            text="uncompress",
+            command=self._handle_uncompression,
+            style="Custom.TButton"
+        )
 
         self._compression_var = IntVar()
         radiobutton_label = ttk.Label(
-            master=self._buttons_frame,
+            master=self._compression_frame,
             text="Select compression method",
             style="Custom.TLabel"
         )
 
         radiobutton_huffman = ttk.Radiobutton(
-            self._buttons_frame,
+            self._compression_frame,
             text="Huffman coding",
             variable=self._compression_var,
             value=1,
@@ -118,57 +183,39 @@ class CompressionView:
             state=self._state)
 
         radiobutton_lempelziv77 = ttk.Radiobutton(
-            self._buttons_frame,
+            self._compression_frame,
             text="Lempel-Ziv 77",
             variable=self._compression_var,
             value=2,
             style="Custom.TRadiobutton",
             state=self._state)
 
-        radiobutton_label.grid(row=2, column=0, pady=10, sticky=constants.W)
-        radiobutton_huffman.grid(row=3, column=0, sticky=constants.W)
-        radiobutton_lempelziv77.grid(row=4, column=0, sticky=constants.W)
-
-    def _construct_buttons(self):
-
-        button_select_file = ttk.Button(
-            master=self._buttons_frame,
-            text="select file",
-            command=self._handle_get_file,
-            style="Custom.TButton"
-
+        button_select_file_to_compress.grid(
+            sticky=constants.NSEW
         )
-
-        button_compress = ttk.Button(
-            master=self._buttons_frame,
-            text="compress",
-            command=self._handle_compression,
-            style="Custom.TButton"
-        )
-
-        button_uncompress = ttk.Button(
-            master=self._buttons_frame,
-            text="uncompress",
-            command=self._handle_uncompression,
-            style="Custom.TButton"
-        )
-
-        button_select_file.grid(
-            row=1,
-            column=0,
-            sticky=constants.W
-        )
+        
+        radiobutton_label.grid(
+            pady=10,
+            sticky=constants.W)
+        
+        radiobutton_huffman.grid(
+            sticky=constants.W)
+        
+        radiobutton_lempelziv77.grid(
+            sticky=constants.W)
 
         button_compress.grid(
-            row=5,
-            column=0,
-            pady=20
+            pady=20,
+            sticky=constants.NSEW
+        )
+
+        button_select_file_to_uncompress.grid(
+            sticky=constants.NSEW
         )
 
         button_uncompress.grid(
-            row=5,
-            column=1,
-            pady=20
+            pady=20,
+            sticky=constants.NSEW
         )
 
     def _construct_log_frame(self):
@@ -183,8 +230,12 @@ class CompressionView:
             wrap="word",
             bg="white"
         )
+        analysis_header.grid(
+            row=1,
+            column=0
+        )
         textfield.grid(
-            row=0,
+            row=1,
             column=0
         )
 
@@ -199,18 +250,18 @@ class CompressionView:
 
         Only validations at the moment are that file is chosen and file extension matches.
         """
-        if not self.filename:
+        if not self.file_to_compress:
             self._file_error("Select a file to compress")
             return
-        if not self.compression_management.validate_file_extension(self.filename[-3:], "txt"):
+        if not self.compression_management.validate_file_extension(self.file_to_compress[-3:], "txt"):
             self._file_error("Can only compress txt-files currently")
             return
         compression_method = self._compression_var.get()
         if compression_method == 1:
             self.compression_management.initial_huffman_compression(
-                self.filename)
+                self.file_to_compress)
         if compression_method == 2:
-            self.compression_management.lempel_ziv_compress(self.filename)
+            self.compression_management.lempel_ziv_compress(self.file_to_compress)
         self._update_log()
         self._compression_status_notification("File compressed successfully!")
 
@@ -237,43 +288,57 @@ class CompressionView:
         Only validations at the moment are that file is chosen and file extension matches.
         """
 
-        compression_method = self._compression_var.get()
-        if not self.filename:
+        if not self.file_to_uncompress:
             self._file_error("Select a file to uncompress")
             return
-        if compression_method == 1 and not self.compression_management.validate_file_extension(self.filename[-3:], "huf"):
-            self._file_error("Can only uncompress huf- and lz-files currently")
+        if not (self.file_to_uncompress[-3:] == "huf" or self.file_to_uncompress[-2:] == "zv"):
+            self._file_error("Can only uncompress .huf and .zv - files.")
             return
-        if compression_method == 2 and not self.compression_management.validate_file_extension(self.filename[-2:], "lz"):
-            self._file_error("Can only uncompress huf- and lz-files currently")
-            return
-        if compression_method == 1:
+        if self.file_to_uncompress[-3:] == "huf":
             self.compression_management.initial_huffman_uncompression(
-                self.filename)
-        if compression_method == 2:
+                self.file_to_uncompress)
+        if self.file_to_uncompress[-2:] == 2:
             self.compression_management.lempel_ziv_uncompress(
-                self.filename
+                self.file_to_uncompress
             )
         self._update_log()
         self._compression_status_notification(
             "File uncompressed successfully!")
 
-    def _handle_get_file(self):
+    def _handle_get_file_to_compress(self):
         """A method to handle filedialog opening.
 
         Default path can be configured in .env - file.
         """
 
-        self.filename = filedialog.askopenfilename(initialdir=DEFAULT_DATA_PATH,
+        self.file_to_compress = filedialog.askopenfilename(initialdir=DEFAULT_DATA_PATH,
                                                    title="Select a File",
                                                    filetypes=(
                                                        ("Supported types", ".txt"),
+                                                       ("all files", ".*")
+                                                   )
+                                                   )
+        filename_split = self.file_to_compress.split("/")
+        filename = filename_split[-1]
+        self.label_file_explorer_compress.configure(text=f"File Opened: {filename}")
+
+    def _handle_get_file_to_uncompress(self):
+        """A method to handle filedialog opening.
+
+        Default path can be configured in .env - file.
+        """
+
+        self.file_to_uncompress = filedialog.askopenfilename(initialdir=DEFAULT_DATA_PATH,
+                                                   title="Select a File",
+                                                   filetypes=(
                                                        ("Supported types", ".huf"),
                                                        ("Supported types", ".lz"),
                                                        ("all files", ".*")
                                                    )
                                                    )
-        self.label_file_explorer.configure(text="File Opened: "+self.filename)
+        filename_split = self.file_to_uncompress.split("/")
+        filename = filename_split[-1]
+        self.label_file_explorer_uncompress.configure(text=f"File Opened: {filename}")
 
     def _file_error(self, content: str):
         """A general method to handle showing messageboxes with an error
