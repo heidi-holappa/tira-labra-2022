42f2dac7a0f532a8a7683e398fc49a5b692bcbb4
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-06-17 00:53:24 +0300

    Add graph-image folder

--- src/config.py
+++ src/config.py
@@ -10,6 +10,8 @@ except FileNotFoundError:
 
 path = os.getenv('DEFAULT_DATA_DIRECTORY') or 'data'
 testing_path = os.getenv('DEFAULT_EXTENSIVE_TESTING_DIRECTORY') or 'test-data'
+graph_directory = os.getenv('DEFAULT_HTML_REPORT_IMAGES') or 'test-data/images'
 
 DEFAULT_DATA_PATH = os.path.join(dirname, '..', path)
 DEFAULT_TEST_DATA_PATH = os.path.join(dirname, '..', testing_path)
+DEFAULT_TEST_GRAPH_FOLDER = os.path.join(dirname, '..', graph_directory)
