4206c51716c718b8caa1eaf857c1f7667406d88d
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-06-19 22:52:46 +0300

    Add new tests

--- src/tests/extensive_test.py
+++ src/tests/extensive_test.py
@@ -1,8 +1,9 @@
 import unittest
 import os
 import string
-from config import DEFAULT_TEST_DATA_PATH
+from config import DEFAULT_TEST_DATA_PATH, DEFAULT_TEST_GRAPH_FOLDER
 from config import HTML_LOG, CSV_LOG, ARCHIVE_LOG, TKINTER_LOG
+from config import IMG_COMPRESS_RATIO, IMG_HUFFMAN_FREQ, IMG_LZ_MEAN_MATCH, IMG_LZ_MEAN_OFFSET
 from services.compressionmanagement import default_compression_management
 from services.extensivetesthandler import default_test_handler
 
@@ -83,11 +84,34 @@ class TestExtensiveTesting(unittest.TestCase):
             content_one, content_two)
         self.assertEqual(False, is_valid)
 
-    def test_activated_extensive_tests_create_an_html_log(self):
+    def test_activated_extensive_tests_create_an_csv_and_html_log(self):
         self.extensive_test_handler.activate_extensive_tests(1, 10000)
-        filepath = os.path.join(DEFAULT_TEST_DATA_PATH, HTML_LOG)
+        result = True
+        for filename in [HTML_LOG, CSV_LOG]:
+            filepath = os.path.join(DEFAULT_TEST_DATA_PATH, filename)
+            result = os.path.exists(filepath)
+            if not result:
+                break
+        self.assertEqual(result, True)
+    
+    def test_old_compression_log_is_archived_when_new_log_is_created(self):
+        self.extensive_test_handler.activate_extensive_tests(1,10000)
+        self.extensive_test_handler.activate_extensive_tests(1,10000)
+        filepath = os.path.join(DEFAULT_TEST_DATA_PATH, ARCHIVE_LOG)
         result = os.path.exists(filepath)
         self.assertEqual(result, True)
+
+    
+    def test_graphs__defined_in_configuration_file_are_created(self):
+        self.extensive_test_handler.activate_extensive_tests(1, 10000)
+        result = True
+        for filename in [IMG_COMPRESS_RATIO, IMG_HUFFMAN_FREQ, IMG_LZ_MEAN_MATCH, IMG_LZ_MEAN_OFFSET]:
+            filepath = os.path.join(DEFAULT_TEST_GRAPH_FOLDER, filename)
+            result = os.path.exists(filepath)
+            if not result:
+                break
+        self.assertEqual(result, True)
+
 
 
     def destroy_test_files(self):
@@ -95,6 +119,10 @@ class TestExtensiveTesting(unittest.TestCase):
         csv_path = os.path.join(DEFAULT_TEST_DATA_PATH, CSV_LOG)
         archive_path = os.path.join(DEFAULT_TEST_DATA_PATH, ARCHIVE_LOG)
         tkinter_log_path = os.path.join(DEFAULT_TEST_DATA_PATH, TKINTER_LOG)
+        img_compression_ratio_path = os.path.join(DEFAULT_TEST_GRAPH_FOLDER, IMG_COMPRESS_RATIO)
+        img_huffman = os.path.join(DEFAULT_TEST_GRAPH_FOLDER, IMG_HUFFMAN_FREQ)
+        img_lz_match = os.path.join(DEFAULT_TEST_GRAPH_FOLDER, IMG_LZ_MEAN_MATCH)
+        img_lz_offset = os.path.join(DEFAULT_TEST_GRAPH_FOLDER, IMG_LZ_MEAN_OFFSET)
         if os.path.exists(html_path):
             os.remove(html_path)
         if os.path.exists(archive_path):
@@ -103,4 +131,12 @@ class TestExtensiveTesting(unittest.TestCase):
             os.remove(csv_path)
         if os.path.exists(tkinter_log_path):
             os.remove(tkinter_log_path)
+        if os.path.exists(img_compression_ratio_path):
+            os.remove(img_compression_ratio_path)
+        if os.path.exists(img_huffman):
+            os.remove(img_huffman)
+        if os.path.exists(img_lz_match):
+            os.remove(img_lz_match)
+        if os.path.exists(img_lz_offset):
+            os.remove(img_lz_offset)
 
