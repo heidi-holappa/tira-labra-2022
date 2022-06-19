4270df5b3e53faa804f9d18172cb9e71eb7a8e35
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-06-19 22:10:37 +0300

    Add configurable graph filenames

--- src/config.py
+++ src/config.py
@@ -20,3 +20,8 @@ HTML_LOG = os.getenv("HTML_LOG_FILE") or "compression-log.html"
 CSV_LOG = os.getenv("CSV_LOG") or "compression-log.csv"
 ARCHIVE_LOG = os.getenv("LOG_ARCHIVE_FILE") or "compression_archive.log"
 TKINTER_LOG = os.getenv("TKINTER_LOG_CONTENT") or "compression.log"
+
+IMG_COMPRESS_RATIO = os.getenv("IMG_COMPRESSION_RATIO") or "test-compression-ratio-comparison.png"
+IMG_HUFFMAN_FREQ = os.getenv("IMG_HUFFMAN_FREQUENCY") or "test-huffman-frequency.variance.png"
+IMG_LZ_MEAN_MATCH = os.getenv("IMG_LEMPEL_ZIV_MEAN_MATCH") or "test-lempel-ziv-avg-match.png"
+IMG_LZ_MEAN_OFFSET = os.getenv("IMG_LEMPEL_ZIV_MEAN_OFFSET") or "test-lempel-ziv-avg-offset.png"
