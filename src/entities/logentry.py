268d04cda471065b51f8e46d89d12760ed95b6ba
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-06-27 09:36:40 +0300

    Refactor, write docstring

--- src/entities/logentry.py
+++ src/entities/logentry.py
@@ -1,6 +1,17 @@
 class LogEntry:
+    """Creates an object instance of the current log entry.
+    """
 
     def __init__(self):
+        """Constructor for the class. For the purposes of this project
+        a dictionary containing the log values was sufficient. There are
+        however quality issues with this solution, as updating the code
+        contains risks, because the data is fetched from a csv without
+        value keys.
+
+        A good upgrade would be to include the keys into the start of
+        the log and the match values with keys when retrieving data.
+        """
         self.logdata = {
             "compression_method": "",
             "original_filename": "",
@@ -22,39 +33,15 @@ class LogEntry:
             "huffman-character-count": ""
         }
 
-    def get_logdata_as_csv_row(self):
+    def get_logdata_as_csv_row(self) -> str:
+        """Returns a csv-compatible version of the logdata.
+
+        Returns:
+            str: logdata separated with semicolons.
+        """
         logdata_as_list = []
         for value in self.logdata.values():
             logdata_as_list.append(value)
         csv_data = ";".join(logdata_as_list)
         return csv_data
 
-    def get_test_data_as_csv_row(self):
-        logdata_as_list = []
-        for key, value in self.logdata.items():
-            if key == "uncompressed_filename":
-                continue
-            logdata_as_list.append(value)
-        csv_data = ";".join(logdata_as_list)
-        return csv_data
-
-
-# if __name__ == "__main__":
-    # l = LogEntry()
-    # l.logdata["compression_method"] = "1"
-    # l.logdata["original_filename"] = "file.txt"
-    # l.logdata["compressed_filename"] = "file.lz"
-    # l.logdata["uncompressed_filename"] = "file_uncompressed.txt"
-    # l.logdata["uncompressed_size"] = "23454"
-    # l.logdata["compressed_size"] = "17584"
-    # l.logdata["compression_ratio"] = "75.0"
-    # l.logdata["compression_time"] = "0.75"
-    # l.logdata["uncompression_time"] = "0.02"
-    # l.logdata["data_fetch_and_process_time"] = "2.9"
-    # l.logdata["data_write_and_process_time"] = "0.1"
-    # l.logdata["huffman_max_fequency"] = "0.3"
-    # l.logdata["huffman_min_frequency"] = "0.00001"
-    # l.logdata["huffman_freq_variance"] = "0.23"
-    # l.logdata["lz_avg_match_length"] = "0"
-    # print(l.get_logdata_as_csv_row())
-    # print(l.get_test_data_as_csv_row())
